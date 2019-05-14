4426030e894d8b6c44298caf86172ed0ad93feb9
Ambersrivastava08 <49266154+Ambersrivastava08@users.noreply.github.com>
2019-05-14 15:26:59 +0530

    day2

---
+++ op6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ 
+int main()
+{
+      int num=64 num1, a, count = 0 ;
+      printf("\nEnter First Number:\t");
+      scanf("%d", &a);
+      num1 = a;
+      while(num >= num1)
+      {
+            num = num - num2;
+            count++;
+      }
+      printf("\nDivision of %d and %d:\nQuotient:\t%d\nRemainder:\t%d\n", num,a,  count, num);
+      return 0;
+}
